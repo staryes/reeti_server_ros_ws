d68a149964c8f19929ce6c85d855636d8bbe9223
staryes <staryes@me.com>
2018-11-28 16:50:48 -0500

    combine eye and neck

--- src/face_tracking/src/face_tracking_eye_neck.cpp
+++ src/face_tracking/src/face_tracking_eye_neck.cpp
@@ -8,6 +8,8 @@
 #include <opencv2/objdetect/objdetect.hpp>
 
 #include <face_tracking/reetiEyesPose.h>
+#include <face_tracking/reetiNeckPose.h>
+#include <face_tracking/reetiPose.h>
 
 static const std::string OPENCV_WINDOW = "Image window";
 
@@ -17,7 +19,9 @@ class FaceDetector
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
-  ros::Publisher servo_pos_pub;
+  ros::Publisher eyes_pos_pub_;
+  ros::Publisher neck_pos_pub_;
+    ros::Subscriber reeti_pos_sub_;
 
     //-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
     cv::String face_cascade_name = "/home/shoushan/reeti_server_ros_ws/src/face_tracking/res/haarcascade_frontalface_alt.xml";
@@ -32,26 +36,27 @@ class FaceDetector
     //cv::Mat debugImage;
     cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
 
-  const double focalLength = 685.5;
-  const double centerX = 344.559882;
-  const double centerY = 216.258888;
+  const static float focalLength = 685.5;
+  const static float centerX = 344.559882;
+  const static float centerY = 216.258888;
 
-  int servo_deg_yaw = 50;
-  int servo_deg_pitch= 50;
+  float servo_deg_yaw = 50;
+  float servo_deg_pitch= 50;
 
-  int servo_reeti_yaw = 50;
-  int servo_reeti_pitch = 50;
+  float servo_reeti_yaw = 50;
+  float servo_reeti_pitch = 50;
 
-  int servo_reeti_neck_yaw = 50;
-  int servo_reeti_neck_pitch = 50;
-  int servo_reeti_neck_roll = 50;
+  float servo_reeti_neck_yaw = 50;
+  float servo_reeti_neck_pitch = 50;
+  float servo_reeti_neck_roll = 50;
 
+  int count = 0;
 
-  int x_deg_shift(int x)
+  float x_deg_shift(int x)
   {
-    double d_deg;
+    float d_deg;
 
-    d_deg = (centerX - x) /25 + 0.5;
+    d_deg = (centerX - x) /25; //8 * 3 times slow
 
     //d_deg = atan(d_deg);
     //d_deg = d_deg * 180 / 3.14159;
@@ -60,11 +65,11 @@ class FaceDetector
     return d_deg;
   }
 
-  int y_deg_shift(int y)
+  float y_deg_shift(int y)
   {
-    double d_deg;
+    float d_deg;
 
-    d_deg = (centerY - y) / 25 + 0.5;
+    d_deg = (centerY - y) / 25;
 
     //d_deg = atan(d_deg);
     //d_deg = d_deg * 180 / 3.14159;
@@ -72,31 +77,32 @@ class FaceDetector
 
     return d_deg;
   }
+
 
 
   void send_gaze_point(int x, int y)
   {
-    int d_yaw = x_deg_shift(x);
-    int d_pitch = y_deg_shift(y);
+    float d_yaw = x_deg_shift(x);
+    float d_pitch = y_deg_shift(y);
 
-    int yaw = d_yaw + servo_deg_yaw;
+    float yaw = d_yaw + servo_reeti_yaw;
     if (yaw < 0)
-      yaw = 0;
+      yaw = 0.0;
     if (yaw > 100)
-      yaw = 100;
+      yaw = 100.0;
     servo_deg_yaw = yaw;
 
-    int pitch = d_pitch + servo_deg_pitch;
+    float pitch = d_pitch + servo_reeti_pitch;
     if (pitch < 0)
-      pitch = 0;
+      pitch = 0.0;
     if (pitch > 100)
-      pitch = 100;
+      pitch = 100.0;
     servo_deg_pitch = pitch;
 
-    ROS_INFO("yaw: %d, pitch: %d", yaw, pitch);
+    ROS_INFO("dYaw: %f, dPitch: %f", yaw, pitch);
 
-    servo_reeti_yaw = servo_deg_yaw ;
-    servo_reeti_pitch = servo_deg_pitch ;
+    //servo_reeti_yaw = servo_deg_yaw ;
+    //servo_reeti_pitch = servo_deg_pitch ;
 
     //int d_neck_eyes_yaw = 50 - servo_reeti_yaw;
 
@@ -132,7 +138,7 @@ class FaceDetector
         for( int i = 0; i < faces.size(); i++ )
         {
             rectangle(frame, faces[i], 1234);
-            ROS_INFO("p(%d, %d), width: %d, height: %d", faces[i].x, faces[i].y, faces[i].width, faces[i].height);
+            ROS_INFO("face(%d, %d), width: %d, height: %d", faces[i].x, faces[i].y, faces[i].width, faces[i].height);
             send_gaze_point(faces[0].x + (faces[0].width * 0.5), faces[0].y + (faces[0].height * 0.5));
         }
         //imshow(main_window_name, debugImage);
@@ -150,8 +156,10 @@ class FaceDetector
             return -1;
         }
         ROS_INFO("Got face cascade file!");
-        servo_pos_pub = nh_.advertise<face_tracking::reetiEyesPose>("reeti/eyes",1);
+        eyes_pos_pub_ = nh_.advertise<face_tracking::reetiEyesPose>("reeti/eyes",1);
+        neck_pos_pub_ = nh_.advertise<face_tracking::reetiNeckPose>("reeti/neck",1);
         //        servo_pos_pub = nh_.advertise<std_msgs::UInt8MultiArray>("neck_eyes",1);
+        reeti_pos_sub_ = nh_.subscribe("reeti/reetiPose", 1, &FaceDetector::reetiPoseCallback, this);
 
         return 0;
     }
@@ -174,6 +182,73 @@ public:
   {
     cv::destroyWindow(OPENCV_WINDOW);
   }
+
+    void reetiPoseCallback(const face_tracking::reetiPose& msg)
+    {
+        servo_reeti_yaw = msg.leftEyeYaw;
+        servo_reeti_pitch = msg.leftEyePitch;
+        //ROS_INFO("left eye: %f, %f", servo_reeti_yaw, servo_reeti_pitch);
+
+        servo_reeti_neck_yaw = msg.neckYaw;
+        servo_reeti_neck_pitch = msg.neckPitch;
+        servo_reeti_neck_roll = msg.neckRoll;
+        // ROS_INFO("neck: %f, %f, %f", servo_reeti_neck_yaw, servo_reeti_neck_pitch, servo_reeti_neck_roll);
+
+            count++;
+//  if (count < 40)
+
+//    if (count < 40)
+//    {
+//    }
+    if (count == 5)
+    {
+        face_tracking::reetiNeckPose neck_msg;
+
+        neck_msg.header.stamp = ros::Time::now();
+        neck_msg.header.frame_id = "/world";
+
+        neck_msg.neckYaw = 50;
+        neck_msg.neckPitch = 50;
+        neck_msg.neckRoll = 50;
+
+        ROS_INFO("Neck!");
+
+        neck_pos_pub_.publish(neck_msg);
+    }
+    else if (count == 10)
+    {
+        count = 0;
+        face_tracking::reetiNeckPose neck_msg;
+
+        neck_msg.header.stamp = ros::Time::now();
+        neck_msg.header.frame_id = "/world";
+
+        neck_msg.neckYaw = 40;
+        neck_msg.neckPitch = 50;
+        neck_msg.neckRoll = 50;
+
+        ROS_INFO("Neck! Back!");
+
+        neck_pos_pub_.publish(neck_msg);
+    }
+    else
+    {
+        face_tracking::reetiEyesPose eyes_msg;
+
+        eyes_msg.header.stamp = ros::Time::now();
+        eyes_msg.header.frame_id = "/world";
+
+        eyes_msg.rightEyeYaw = servo_deg_yaw + 30;
+        eyes_msg.rightEyePitch = servo_deg_pitch;
+        eyes_msg.leftEyeYaw = servo_deg_yaw;
+        eyes_msg.leftEyePitch = servo_deg_pitch;
+
+        ROS_INFO("rYaw: %f, rPitch: %f", servo_reeti_yaw, servo_reeti_pitch);
+
+        eyes_pos_pub_.publish(eyes_msg);
+    }
+    }
+
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
@@ -220,19 +295,6 @@ public:
     // array.data[5] = servo_reeti_yaw;
     // array.data[6] = servo_reeti_pitch;
 
-    face_tracking::reetiEyesPose eyes_msg;
-
-    eyes_msg.header.stamp = ros::Time::now();
-    eyes_msg.header.frame_id = "/world";
-
-    eyes_msg.rightEyeYaw = servo_reeti_yaw + 30;
-    eyes_msg.rightEyePitch = servo_reeti_pitch;
-    eyes_msg.leftEyeYaw = servo_reeti_yaw;
-    eyes_msg.leftEyePitch = servo_reeti_pitch;
-
-    ROS_INFO("(%d, %d)", servo_reeti_yaw, servo_reeti_pitch);
-
-    servo_pos_pub.publish(eyes_msg);
 
   }
 };
