1f170d02c2fbfeb14e25ba57e3f2fafef0aabe3a
shoushan <shoushan@OpenMacBookPro.local>
2018-11-28 21:24:04 -0500

    refactoring

--- src/face_tracking/src/static_image_insert_pub.cpp
+++ src/face_tracking/src/static_image_insert_pub.cpp
@@ -19,18 +19,21 @@ class ImagePublisher
     cv::String image2_name = "/home/staryes/Desktop/CIMG0178.JPG";
     cv::String image0_name = "/home/staryes/Desktop/OperaMeganeko.png";
 
-    cv::Mat image1;
-    cv::Mat image2;
-    cv::Mat image0;
-
-//    sensor_msgs::ImagePtr imsg1;
+    sensor_msgs::ImagePtr imsg1;
+    sensor_msgs::ImagePtr imsg2;
+    sensor_msgs::ImagePtr imsg0;
 //    sensor_msgs::ImagePtr
 
     void loadImages(void)
     {
-        this->image1 = cv::imread(this->image1_name);
-        this->image2 = cv::imread(this->image2_name);
-        this->image0 = cv::imread(this->image0_name);
+        cv::Mat image1 = cv::imread(this->image1_name);
+        sensor_msgs::ImagePtr imsg1= cv_bridge::CvImage(std_msgs::Header(), "bgr8", image1).toImageMsg();
+
+        cv::Mat image2 = cv::imread(this->image2_name);
+        sensor_msgs::ImagePtr imsg2 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image2).toImageMsg();
+
+        cv::Mat image0 = cv::imread(this->image0_name);
+        sensor_msgs::ImagePtr imsg0 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image0).toImageMsg();
     }
 
 
@@ -53,26 +56,17 @@ public:
         ros::Duration(0.5).sleep(); // sleep for half a second
         if (msg->data == 1)
         {
-            sensor_msgs::ImagePtr imsg1= cv_bridge::CvImage(std_msgs::Header(), "bgr8", image1).toImageMsg();
             image_pub_.publish(imsg1);
 
             ros::Duration(0.2).sleep();
-
         }
         else if (msg->data == 2)
         {
-            sensor_msgs::ImagePtr imsg2 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image2).toImageMsg();
             image_pub_.publish(imsg2);
             ros::Duration(0.2).sleep();
         }
 
-
-        {
-            sensor_msgs::ImagePtr imsg0 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image0).toImageMsg();
-            image_pub_.publish(imsg0);
-        }
-
-
+        image_pub_.publish(imsg0);
     }
 
 
@@ -87,34 +81,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
-/*
-    cv_bridge::CvImage cv_image;
-    cv_image.image = cv::imread("/home/staryes/Desktop/OperaMeganeko.png", CV_LOAD_IMAGE_COLOR);
-    cv_image.encoding = "bgr8";
-    sensor_msgs::Image ros_image;
-    cv_image.toImageMsg(ros_image);
-
-    cv_bridge::CvImage ano_image;
-    ano_image.image = cv::imread("/home/staryes/Desktop/CIMG0178.JPG", CV_LOAD_IMAGE_COLOR);
-    ano_image.encoding = "bgr8";
-    sensor_msgs::Image ano_r_image;
-    ano_image.toImageMsg(ano_r_image);
-
-    ros::Publisher pub = nh.advertise<sensor_msgs::Image>("/static_image", 1);
-    ros::Rate loop_rate(5);
-
-    int count = 0;
-    while(nh.ok())
-    {
-        if (count%20 == 0)
-            pub.publish(ano_r_image);
-        else
-            pub.publish(ros_image);
-        loop_rate.sleep();
-
-        count++;
-        //ros::spin();
-    }
-}
-*/
