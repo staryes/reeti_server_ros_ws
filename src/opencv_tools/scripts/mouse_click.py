08c4e7aeb78b43107d70bab567037c8a19dbe51b
staryes <staryes@me.com>
2019-02-06 15:06:18 -0500

    send neck position by clicking

--- src/opencv_tools/scripts/mouse_click.py
+++ src/opencv_tools/scripts/mouse_click.py
@@ -10,6 +10,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import UInt16MultiArray
+from reetiros.msg import reetiNeckPose
 
 class image_converter:
 
@@ -24,12 +25,20 @@ class image_converter:
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/cv_camera/image_raw",Image, self.callback)
     self.point_pub = rospy.Publisher("clicked_point", UInt16MultiArray, queue_size = 1)
+    self.neck_clicked_pub = rospy.Publisher("/reeti/neck", reetiNeckPose, queue_size = 1)
 
   def click_callback(self, event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         msg = UInt16MultiArray()
         msg.data = [x, y]
         self.point_pub.publish(msg)
+        rospy.loginfo("(%d, %d) is clicked ", x, y)
+        neck_msg = reetiNeckPose()
+        neck_msg.neckYaw = (x * -0.1) + 85
+        neck_msg.neckPitch = 50
+        neck_msg.neckRoll = (y * -0.3) + 133
+        self.neck_clicked_pub.publish(neck_msg)
+        
     
   def callback(self,data):
     try:
