b5ddfdc9573af6c5eb782baae86c35ce1e4e7950
staryes <staryes@me.com>
2019-02-08 13:32:54 -0500

    add motion center in python

---
+++ src/reeti_parser_server/scripts/reeti_motion_server.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+
+import rospy
+from std_msgs.msg import UInt16MultiArray
+from std_msgs.msg import String
+from reetiros.srv import *
+
+
+#def image2reetineck(x, y):
+
+def handle_neck_motion(req):
+    print ""
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + ' I heard (%s, %s)', data.data[0], data.data[1])
+    hello_str = "hello world %s" % rospy.get_time()
+    rospy.loginfo(hello_str)
+    global pub
+    pub.publish(hello_str)
+    
+def neck_pos_pub():
+    global pub
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+#    rospy.init_node('talker', anonymous=True)
+    #rate = rospy.Rate(10) # 10hz
+    #while not rospy.is_shutdown():
+
+     #   rate.sleep()
+    
+def get_click_point():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('clicked_point', UInt16MultiArray, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+def reeti_neck_client() :
+    rospy.wait_for_service('MoveNeck')
+    print "wait for MoveNeck"
+    try:
+        move_neck = rospy.ServiceProxy('MoveNeck', MoveNeck )
+        print "connect to service server MoveNeck"
+        #move_neck (neckYaw, neckPitch, neckRoll)
+        move_neck (70, 20, 20)
+        print "send req"
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+if __name__ == '__main__':
+    #neck_pos_pub()
+    #get_click_point()
+    reeti_neck_client()
+    
+
